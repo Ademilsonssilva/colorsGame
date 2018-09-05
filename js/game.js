b4cc5d1ab92fd8555fde776482f4d3314fb48c16
Ademilson <ademilsonssilva1@gmail.com>
2018-09-05 17:49:34 -0300

    Leaderboard incompleto

--- js/game.js
+++ js/game.js
@@ -15,11 +15,47 @@ $(document).ready(function () {
 	});
 
 	$('#difficulty_leaderboards').on('change', function () {
-		asd = firebase.database().ref('scores').orderByChild('moves').equalTo('Easy');
+		
+		tamanhos = [];
+		firebase.database().ref('scores').orderByChild('difficulty').equalTo($('#difficulty_leaderboards option:selected').prop('title')).on('value', function (snapshot) {
+			$('#leaderboard-content').html('');
+
+			snapshot.forEach(function (score) {
+				obj = score.val();
+				
+				// tamanhos.push(parseInt(obj.moves));
+				$('#leaderboard-content').append('<li>' + obj.name + ' - Moves: ' + obj.moves + '</li>');
+
+			});
+		});
+		// console.log(tamanhos);
+		// novoArray = [];
+		// newa = recursive_sort(tamanhos, novoArray);
+		// console.log(newa);
 
 		// $('#difficulty_leaderboards option:selected').prop('title'));
-		console.log(asd);
+		//console.log(asd);
 	})
 
 
 });
+
+// function recursive_sort(array, novoArray, item = 9999)
+// {
+// 	alert(array);
+// 	if (array.length == 0) {
+// 		alert('etou');
+// 		return novoArray;
+// 	}
+// 	for (i= 0; i < array.length; i++) {
+// 		if (array[i] < item) {
+// 			posicao = i;
+// 			item = array[i];
+// 		}
+// 	}
+// 	novoArray.push(item);
+// 	array.splice(posicao);
+
+// 	return recursive_sort(array, novoArray, item);
+// }
+
