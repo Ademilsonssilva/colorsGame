c7557b0877d359361bec539165bab7a7e352434e
Ademilson <ademilsonssilva1@gmail.com>
2018-09-06 16:25:51 -0300

    Correções no leaderboard

--- js/game.js
+++ js/game.js
@@ -17,45 +17,17 @@ $(document).ready(function () {
 	$('#difficulty_leaderboards').on('change', function () {
 		
 		tamanhos = [];
-		firebase.database().ref('scores').orderByChild('difficulty').equalTo($('#difficulty_leaderboards option:selected').prop('title')).on('value', function (snapshot) {
+		firebase.database().ref('scores').orderByChild('moves').on('value', function (snapshot) {
 			$('#leaderboard-content').html('');
 
 			snapshot.forEach(function (score) {
 				obj = score.val();
-				
-				// tamanhos.push(parseInt(obj.moves));
-				$('#leaderboard-content').append('<li>' + obj.name + ' - Moves: ' + obj.moves + '</li>');
-
+				if ($('#difficulty_leaderboards option:selected').prop('title') == obj.difficulty) {
+					$('#leaderboard-content').append('<li>' + obj.name + ' - Moves: ' + obj.moves + '</li>');
+				}	
 			});
 		});
-		// console.log(tamanhos);
-		// novoArray = [];
-		// newa = recursive_sort(tamanhos, novoArray);
-		// console.log(newa);
-
-		// $('#difficulty_leaderboards option:selected').prop('title'));
-		//console.log(asd);
 	})
 
 
 });
-
-// function recursive_sort(array, novoArray, item = 9999)
-// {
-// 	alert(array);
-// 	if (array.length == 0) {
-// 		alert('etou');
-// 		return novoArray;
-// 	}
-// 	for (i= 0; i < array.length; i++) {
-// 		if (array[i] < item) {
-// 			posicao = i;
-// 			item = array[i];
-// 		}
-// 	}
-// 	novoArray.push(item);
-// 	array.splice(posicao);
-
-// 	return recursive_sort(array, novoArray, item);
-// }
-
